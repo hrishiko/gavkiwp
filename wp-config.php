b040d7645714c0178368813292836f3c249bd2a1
admin <tanveersure@couragedigital.com>
2016-03-01 17:04:27 +0530

    Plugin Revisr Updated

--- wp-config.php
+++ wp-config.php
@@ -20,6 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('REVISR_GIT_PATH', 'cmd/git'); // Added by Revisr
 define('DB_NAME', 'appcom_wp595');
 
 /** MySQL database username */
